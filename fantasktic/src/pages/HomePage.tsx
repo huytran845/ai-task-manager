9357556e95428e5d437f3911a52fcd25b67dc0b3
huytran845~ <huytrit@gmail.com>
2025-03-28 18:32:54 -0700

    Completed Design for Homepage

--- fantasktic/src/pages/HomePage.tsx
+++ fantasktic/src/pages/HomePage.tsx
@@ -14,14 +14,19 @@ const HomePage = () => {
       <Head title="Fantasktic - AI Creates Your Tasks So You Don't Have To!" />
 
       <section>
-        <div className="">
-          <div className="">
-            <h1 className="">
-              From Chaos to Clarity, Stay Organized with{" "}
-              <span className="">AI-Powered</span> Task Management.
+        <div className="container !px-8 grid grid-cols-1 gap-8 items-center xl:gap-12 xl:grid-cols-[1fr_1.5fr]">
+          <div className="flex flex-col items-center text-center space-y-4 lg:text-left lg:items-start lg:space-y-6">
+            <h1 className="text-4xl font-semibold max-w-[22ch] md:text-5xl lg:text-6xl xl:text-5xl 2xl:text-6xl">
+              From Chaos to Clarity,
+              <br /> Stay Organized with <br />
+              <span className="inline-flex mt-2 bg-gradient-to-t from-primary/50 to-primary/30 rounded-full px-2 overflow-hidden">
+                AI-Powered
+              </span>
+              <br />
+              Task Management.
             </h1>
 
-            <p className="">
+            <p className="max-w-[48ch] text-foreground/80 md:text-lg lg:text-xl">
               Conquer your projects while ensuring that you and your team can
               stay on track with this Fantasktic to-do list app.
             </p>
