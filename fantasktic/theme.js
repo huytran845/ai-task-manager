2f99a4dda462bae47c191fc2b7dc1fe0847edb0a
huytran845~ <huytrit@gmail.com>
2025-08-07 15:48:54 -0700

    Converted theme script file to javascript

--- fantasktic/theme.js
+++ fantasktic/theme.js
@@ -1,5 +1,5 @@
 // Function to set theme based on user preference
-function applyTheme(): void {
+function applyTheme() {
   // If you want theme to be constant and not swap to user's system preference.
   // Check if a preference is already saved
   // const savedTheme: string | null = localStorage.getItem("theme");
@@ -7,10 +7,8 @@ function applyTheme(): void {
   //   document.documentElement.className = savedTheme;
   // } else {
   // Check system preference if no saved theme
-  const prefersDark: boolean = window.matchMedia(
-    "(prefers-color-scheme: dark)",
-  ).matches;
-  const systemTheme: string = prefersDark ? "dark" : "light";
+  const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const systemTheme = prefersDark ? "dark" : "light";
   document.documentElement.className = systemTheme;
   // Saves the theme to local storage for other components
   localStorage.setItem("theme", systemTheme);
@@ -18,9 +16,9 @@ function applyTheme(): void {
 }
 
 // Function to change the theme from its current
-function toggleTheme(): void {
-  const currentTheme: string = document.documentElement.className;
-  const newTheme: string = currentTheme === "dark" ? "light" : "dark";
+function toggleTheme() {
+  const currentTheme = document.documentElement.className;
+  const newTheme = currentTheme === "dark" ? "light" : "dark";
   document.documentElement.className = newTheme;
   localStorage.setItem("theme", newTheme);
 }
